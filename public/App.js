d3e3302e4ed612ca348e0d088cfffc7f0a054c5e
horsaytale <98501452+horsaytale@users.noreply.github.com>
2024-01-02 20:17:49 +0800

    Add files via upload

---
+++ public/App.js
@@ -0,0 +1,79 @@
+const skills = [
+  {
+    skill: "HTML+CSS",
+    level: "advanced",
+    color: "#2662EA",
+  },
+  {
+    skill: "JavaScript",
+    level: "intermediate",
+    color: "#EFD81D",
+  },
+  {
+    skill: "Web Design",
+    level: "advanced",
+    color: "#C3DCAF",
+  },
+  {
+    skill: "Git and GitHub",
+    level: "intermediate",
+    color: "#E84F33",
+  },
+  {
+    skill: "React",
+    level: "beginner",
+    color: "#60DAFB",
+  },
+];
+
+export default function App() {
+  return (
+    <div className="card">
+      <Avatar />
+      <div className="data">
+        <Intro />
+        <SkillList />
+      </div>
+    </div>
+  );
+}
+
+function Avatar() {
+  return <img src="self-bio.jpg" className="avatar" alt="Name of Bio" />;
+}
+
+function Intro() {
+  return (
+    <div>
+      <h1>Amber Lim</h1>
+      <p>Web developer.</p>
+      <p>
+        When not coding, I enjoy listening to songs, play some cool RPG games
+        and read some spooky horror story books.
+      </p>
+    </div>
+  );
+}
+
+function SkillList() {
+  return (
+    <div className="skill-list">
+      {skills.map((skill) => (
+        <Skill skill={skill.skill} color={skill.color} level={skill.level} />
+      ))}
+    </div>
+  );
+}
+
+function Skill({ skill, level, color }) {
+  return (
+    <div className="skill" style={{ backgroundColor: color }}>
+      <span>{skill}</span>
+      <span>
+        {level === "beginner" && "👶"}
+        {level === "intermediate" && "💪"}
+        {level === "advanced" && "👍"}
+      </span>
+    </div>
+  );
+}
